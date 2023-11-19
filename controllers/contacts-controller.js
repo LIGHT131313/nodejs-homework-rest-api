4916dbecff5ee8e2352185a3dc0312eaab8d0376
LIGHT131313 <131313light@gmail.com>
2023-11-19 17:13:45 +0200

    done

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -1,17 +1,17 @@
-import contactService from "../models/contacts/index.js";
+import Contact from "../models/Contact.js";
 
 import { ctrlWrapper } from "../decorators/index.js";
 
 import { HttpError } from "../helpers/index.js";
 
 const getAll = async (req, res) => {
-  const result = await contactService.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
-  const result = await contactService.getContactById(id);
+  const result = await Contact.findById(id);
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
@@ -19,13 +19,13 @@ const getById = async (req, res) => {
 };
 
 const add = async (req, res) => {
-  const result = await contactService.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const updateById = async (req, res) => {
   const { id } = req.params;
-  const result = await contactService.updateContact(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body);
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
@@ -34,7 +34,7 @@ const updateById = async (req, res) => {
 
 const deleteById = async (req, res) => {
   const { id } = req.params;
-  const result = await contactService.removeContact(id);
+  const result = await Contact.findByIdAndDelete(id);
   if (!result) {
     throw HttpError(404, `Contact with id=${id} not found`);
   }
--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -2,19 +2,20 @@ import express from "express";
 
 import contactsControllers from "../../controllers/contacts-controller.js";
 
-import { isEmptyBody } from "../../middlewares/index.js";
+import { isEmptyBody, isValidId } from "../../middlewares/index.js";
 
 import { validateBody } from "../../decorators/index.js";
 import {
   contactAddSchema,
   contactUpdateSchema,
-} from "../../schemas/contact-schemas.js";
+  contactFavoriteSchema,
+} from "../../models/Contact.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", contactsControllers.getAll);
 
-contactsRouter.get("/:id", contactsControllers.getById);
+contactsRouter.get("/:id", isValidId, contactsControllers.getById);
 
 contactsRouter.post(
   "/",
@@ -23,13 +24,22 @@ contactsRouter.post(
   contactsControllers.add
 );
 
-contactsRouter.delete("/:id", contactsControllers.deleteById);
-
 contactsRouter.put(
   "/:id",
+  isValidId,
   isEmptyBody,
   validateBody(contactUpdateSchema),
   contactsControllers.updateById
 );
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody,
+  validateBody(contactFavoriteSchema),
+  contactsControllers.updateById
+);
+
+contactsRouter.delete("/:id", isValidId, contactsControllers.deleteById);
 
 export default contactsRouter;
